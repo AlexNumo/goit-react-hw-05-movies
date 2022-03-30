c23c2cfa2ae879daa11d8ac2dd033a83ab4a0b2c
AlexNumo <korol1511@gmail.com>
2022-03-30 09:42:58 +0300

    Change Go Back

--- src/Pages/PreviewPage/PreviewPage.jsx
+++ src/Pages/PreviewPage/PreviewPage.jsx
@@ -9,14 +9,21 @@ export const PreviewPage = () => {
   // const location = useLocation();
   const [item, setItem] = useState(null);
   const navigate = useNavigate();
+  // const [clicks, setClicks] = useState(0);
   // const [searchParams, setSearchParams] = useSearchParams();
-
   // const search = setSearchParams.get('cast');
-
   // console.log(search);
 
   const goBack = () => navigate(-1);
 
+//   const goBack = () => {
+//   if(clicks === 0 ?? null) {
+//     navigate(-1)
+//     console.log("-1")
+//   };
+//   navigate(-2)
+//   console.log("-2")
+// }
   useEffect(() => {
     async function fetchItem(){
       try{
@@ -29,23 +36,31 @@ export const PreviewPage = () => {
     fetchItem()
   }, [itemId])
 
+  // clicks(0);
+
+  // const clicksBTN = () => {
+  //   setClicks(clicks + 1);
+  // }
+  // console.log(clicks);
+
   return (
     <div>
       {/* <Link to={location?.state?.from ?? '/'}>Go Back</Link> */}
       <button onClick={goBack}>Go Back</button>
+      {/* <button onClick={clicksBTN}>Clicks: </button> */}
       {item && <MovieCard item={item}/>}
       <div>
         <p>Additional information</p>
         <ul>
           <li>
           {/* state={{ from: location?.state?.from ?? '/' }} */}
-            <NavLink to='cast' >
+            <NavLink to='cast'>
               Cast
             </NavLink>
           </li>
           <li>
           {/* state={{ from: location?.state?.from ?? '/' }} */}
-            <NavLink to='reviews' >
+            <NavLink to='reviews'>
               Reviews
             </NavLink>
           </li>
